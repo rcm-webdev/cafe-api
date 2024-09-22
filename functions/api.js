ea7fec968dfe89de85400a5de252d0a690b7d6ca
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-09-22 11:19:46 -0700

    update server file

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,6 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const path = require("path");
-const favicon = require("serve-favicon");
 
 const app = express();
 app.use(cors());
