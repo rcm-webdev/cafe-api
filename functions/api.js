250c68fa4cb8401edbbda34df5d3bd1ac4dfbea3
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-09-22 11:08:48 -0700

    update server favicon

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
